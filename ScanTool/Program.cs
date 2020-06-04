dab6fa2901576acbc1f8b65a487828c2d3675969
Haus <wildenha@cse.msu.edu>
2020-06-04 11:45:26 -0400

    Remove debug hashing code

--- ScanTool/Program.cs
+++ ScanTool/Program.cs
@@ -9,9 +9,6 @@ namespace ScanTool
   {
     static async Task Main( string[] args )
     {
-      var hashes = await HashTool.HashAsync(@"D:\WiiTools\nNASOS.exe", HashType.MD5, HashType.SHA1 );
-      foreach( var hash in hashes )
-        Console.WriteLine( "{0,-5}: {1}", hash.Type, hash.Value );
     }
   }
 }
