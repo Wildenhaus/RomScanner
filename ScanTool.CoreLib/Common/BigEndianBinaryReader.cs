ce28aeb9a59d02f7fdfaee31613a4c6137eb2715
Haus <wildenha@cse.msu.edu>
2020-06-09 15:03:27 -0400

    Add ELF debug symbol scanner (credits to aerosoul)

---
+++ ScanTool.CoreLib/Common/BigEndianBinaryReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace ScanTool.CoreLib
+{
+
+  public sealed unsafe class BigEndianBinaryReader : BinaryReader
+  {
+
+    // TODO: This is just stubbed in for now.
+
+    #region Constructor
+
+    public BigEndianBinaryReader( Stream input )
+      : base( input )
+    {
+    }
+
+    #endregion
+
+    public override ushort ReadUInt16()
+    {
+      var buffer = stackalloc byte[2];
+      var span = new Span<byte>( buffer, 2 );
+
+      Read( span );
+      span.Reverse();
+
+      return BitConverter.ToUInt16( span );
+    }
+
+    public override uint ReadUInt32()
+    {
+      var buffer = stackalloc byte[4];
+      var span = new Span<byte>( buffer, 4 );
+
+      Read( span );
+      span.Reverse();
+
+      return BitConverter.ToUInt32( span );
+    }
+
+  }
+
+}
---
+++ ScanTool.CoreLib/Scanning/Symbols/ElfDebugSymbolScanner.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+
+namespace ScanTool.CoreLib.Scanning.Symbols
+{
+
+  public static class ElfDebugSymbolScanner
+  {
+
+    public static bool HasDebugSymbols( Stream stream )
+    {
+      stream.Seek( 0, SeekOrigin.Begin );
+
+      using( var reader = new BigEndianBinaryReader( stream ) )
+      {
+        // Read e_shoff
+        stream.Seek( 0x20, SeekOrigin.Begin );
+        var shoff = reader.ReadUInt16();
+
+        // Read e_shentsize and e_shnum
+        stream.Seek( 0x2E, SeekOrigin.Begin );
+        var shentsize = reader.ReadUInt16();
+        var shnum = reader.ReadUInt16();
+
+        for( var i = 0; i < shnum; i++ )
+        {
+          // Calculate section header i offset
+          var sectionOffset = (shoff + (i * shentsize));
+
+          // Seek to section header i, plus the offset of sh_type (4)
+          stream.Seek( sectionOffset + 4, SeekOrigin.Begin );
+
+          // Read sh_type
+          var shtype = reader.ReadUInt32(); // SecHdr + 0x4
+
+          if( shtype == 2 ) // SHT_SYMTAB
+            return true;
+        }
+
+      }
+
+      // ELF does not have debug symbols
+      return false;
+    }
+
+  }
+
+}
