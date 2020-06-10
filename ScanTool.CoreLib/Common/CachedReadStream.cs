1c2fabcab367b8d1e3afbb9b2fd3f0b99a04caf5
Haus <wildenha@cse.msu.edu>
2020-06-10 15:01:45 -0400

    Add support for nested archives

---
+++ ScanTool.CoreLib/Common/CachedReadStream.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+
+namespace ScanTool.CoreLib
+{
+
+  public class CachedReadStream : Stream
+  {
+
+    #region Constants
+
+    const int BUFFER_SIZE = 1024 * 1024;
+
+    #endregion
+
+    #region Data Members
+
+    private readonly Stream _sourceStream;
+    private readonly Stream _cacheStream;
+    private readonly byte[] _buffer;
+
+    private long _sourcePosition;
+    private readonly long _length;
+
+    #endregion
+
+    #region Properties
+
+    public override bool CanRead => true;
+    public override bool CanSeek => true;
+    public override bool CanWrite => false;
+
+    public override long Length => _length;
+
+    public override long Position
+    {
+      get => _cacheStream.Position;
+      set => Seek( value, SeekOrigin.Begin );
+    }
+
+    #endregion
+
+    #region Constructor
+
+    public CachedReadStream( Stream sourceStream, Stream cacheStream, long length = -1 )
+    {
+      _sourceStream = sourceStream;
+      _cacheStream = cacheStream;
+      _buffer = new byte[ BUFFER_SIZE ];
+
+      if( length > -1 )
+        _length = length;
+      else
+        _length = _sourceStream.Length;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public override void Flush()
+    {
+    }
+
+    public override int Read( byte[] buffer, int offset, int count )
+    {
+      EnsureCachedUpTo( _sourcePosition + offset + count );
+      return _cacheStream.Read( buffer, offset, count );
+    }
+
+    public override long Seek( long offset, SeekOrigin origin )
+    {
+      switch( origin )
+      {
+        case SeekOrigin.Begin:
+          EnsureCachedUpTo( offset );
+          return _cacheStream.Seek( offset, SeekOrigin.Begin );
+        case SeekOrigin.Current:
+          EnsureCachedUpTo( Position + offset );
+          return _cacheStream.Seek( offset, origin );
+        case SeekOrigin.End:
+          EnsureCachedUpTo( Length + offset );
+          return _cacheStream.Seek( offset, origin );
+        default:
+          return 0;
+      }
+    }
+
+    public override void SetLength( long value )
+    {
+      throw new NotSupportedException();
+    }
+
+    public override void Write( byte[] buffer, int offset, int count )
+    {
+      throw new NotSupportedException();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void EnsureCachedUpTo( long offset )
+    {
+      offset = Math.Min( offset, _length );
+      if( _sourcePosition >= offset )
+        return;
+
+      var originalPosition = _cacheStream.Position;
+      _cacheStream.Seek( _sourcePosition, SeekOrigin.Begin );
+
+      while( _sourcePosition < offset )
+      {
+        var bytesToRead = ( int ) Math.Min( BUFFER_SIZE, offset - _sourcePosition );
+        var bytesRead = _sourceStream.Read( _buffer, 0, bytesToRead );
+        if( bytesRead == 0 )
+          break;
+
+        _cacheStream.Write( _buffer, 0, bytesRead );
+      }
+
+      _sourcePosition = offset;
+      _cacheStream.Seek( originalPosition, SeekOrigin.Begin );
+    }
+
+    #endregion
+
+  }
+
+}
---
+++ ScanTool.CoreLib/VFS/Devices/ArchiveDevice.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SharpCompress.Archives;
+using SharpCompress.Archives.GZip;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Archives.Tar;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Common.GZip;
+using SharpCompress.Common.Rar;
+using SharpCompress.Common.SevenZip;
+using SharpCompress.Common.Tar;
+using SharpCompress.Common.Zip;
+
+namespace ScanTool.CoreLib.VFS.Devices
+{
+
+  public abstract class ArchiveDevice<TEntry, TVolume> : VfsDevice
+    where TEntry : IArchiveEntry
+    where TVolume : IVolume
+  {
+
+    #region Data Members
+
+    private readonly Stream _archiveStream;
+    private readonly AbstractArchive<TEntry,TVolume> _archive;
+
+    #endregion
+
+    #region Constructor
+
+    protected ArchiveDevice( Stream archiveStream )
+    {
+      _archiveStream = archiveStream;
+      _archive = OpenArchive( archiveStream );
+    }
+
+    #endregion
+
+    #region Overrides
+
+    protected override VfsDirectory BuildFileHierarchy()
+    {
+      _archiveStream.Seek( 0, SeekOrigin.Begin );
+      var root = new ArchiveDirectory( this );
+
+      foreach( var entry in _archive.Entries )
+      {
+        if( entry.IsDirectory )
+          continue;
+
+        if( entry == null || entry.Key == null )
+          continue;
+
+        var parent = ResolveParent( entry, root );
+        new ArchiveFile( entry, this, parent );
+      }
+
+      return root;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    protected abstract AbstractArchive<TEntry, TVolume> OpenArchive( Stream stream );
+
+    private VfsEntry ResolveParent( TEntry entry, VfsEntry root )
+    {
+      var path = SanitizePath( entry.Key );
+      var targetEntryName = GetEntryName( path );
+      var pathParts = path.Split( Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries );
+
+      VfsEntry currentEntry = root;
+      foreach( var part in pathParts )
+      {
+        if( part == targetEntryName )
+          break;
+
+        if( !currentEntry.TryGetChild( part, out var targetEntry ) )
+          targetEntry = new ArchiveDirectory( part, this, currentEntry );
+
+        currentEntry = targetEntry;
+      }
+
+      return currentEntry;
+    }
+
+    private static string GetEntryName( string path )
+      => SanitizePath( path ).Split( Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries ).Last();
+
+    private static string SanitizePath( string path )
+      => path.Replace( Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar );
+
+    #endregion
+
+    #region Child Classes
+
+    internal class ArchiveDirectory : VfsDirectory
+    {
+
+      #region Constructor
+
+      internal ArchiveDirectory( VfsDevice device )
+        : base( $"{Path.DirectorySeparatorChar}", device, null )
+      {
+      }
+
+      internal ArchiveDirectory( string path, VfsDevice device, VfsEntry parent = null )
+        : base( GetEntryName( path ), device, parent )
+      {
+      }
+
+      #endregion
+
+    }
+
+    internal class ArchiveFile : VfsFile
+    {
+
+      #region Data Members
+
+      private readonly TEntry _entry;
+
+      #endregion
+
+      #region Constructor
+
+      public ArchiveFile( TEntry entry, VfsDevice device, VfsEntry parent = null )
+        : base( GetEntryName( entry.Key ), device, parent )
+      {
+        _entry = entry;
+        Size = entry.Size;
+      }
+
+      #endregion
+
+      #region Overrides
+
+      public override Stream GetStream()
+        => _entry.OpenEntryStream();
+
+      #endregion
+
+    }
+
+    #endregion
+
+  }
+
+  public sealed class GZipArchiveDevice : ArchiveDevice<GZipArchiveEntry, GZipVolume>
+  {
+
+    public GZipArchiveDevice( Stream archiveStream )
+      : base( archiveStream )
+    {
+    }
+
+    protected override AbstractArchive<GZipArchiveEntry, GZipVolume> OpenArchive( Stream stream )
+      => GZipArchive.Open( stream );
+
+  }
+
+  public sealed class TarArchiveDevice : ArchiveDevice<TarArchiveEntry, TarVolume>
+  {
+
+    public TarArchiveDevice( Stream archiveStream )
+      : base( archiveStream )
+    {
+    }
+
+    protected override AbstractArchive<TarArchiveEntry, TarVolume> OpenArchive( Stream stream )
+      => TarArchive.Open( stream );
+
+  }
+
+  public sealed class RarArchiveDevice : ArchiveDevice<RarArchiveEntry, RarVolume>
+  {
+
+    public RarArchiveDevice( Stream archiveStream )
+      : base( archiveStream )
+    {
+    }
+
+    protected override AbstractArchive<RarArchiveEntry, RarVolume> OpenArchive( Stream stream )
+      => RarArchive.Open( stream );
+
+  }
+
+  public sealed class SevenZipArchiveDevice : ArchiveDevice<SevenZipArchiveEntry, SevenZipVolume>
+  {
+
+    public SevenZipArchiveDevice( Stream archiveStream )
+      : base( archiveStream )
+    {
+    }
+
+    protected override AbstractArchive<SevenZipArchiveEntry, SevenZipVolume> OpenArchive( Stream stream )
+      => SevenZipArchive.Open( stream );
+
+  }
+
+  public sealed class ZipArchiveDevice : ArchiveDevice<ZipArchiveEntry, ZipVolume>
+  {
+
+    public ZipArchiveDevice( Stream archiveStream )
+      : base( archiveStream )
+    {
+    }
+
+    protected override AbstractArchive<ZipArchiveEntry, ZipVolume> OpenArchive( Stream stream )
+      => ZipArchive.Open( stream );
+
+  }
+
+}
