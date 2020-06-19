fc41ee0b2b6de534817cc837efc852c188eec032
Haus <wildenha@cse.msu.edu>
2020-06-19 17:52:32 -0400

    Update shell app to be a basic working CLI

--- ScanTool/Program.cs
+++ ScanTool/Program.cs
@@ -1,56 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using ScanTool.CoreLib.Scanning;
-using ScanTool.CoreLib.Scanning.Signatures;
-using ScanTool.CoreLib.Scanning.Symbols;
 using ScanTool.CoreLib.VFS.Devices;
 
 namespace ScanTool
 {
+
   class Program
   {
+
     static async Task Main( string[] args )
     {
-      foreach( var iso in Directory.GetFiles( "T:\\PS2\\", "*.iso", SearchOption.AllDirectories ) )
-        Scan( iso );
-    }
+      Console.Title = "ScanTool";
+      if( args.Length < 1 )
+      {
+        Console.WriteLine( "Usage: ScanTool [ISO Path]" );
+        return;
+      }
 
+      var path = args[0];
+      if( !File.Exists( path ) )
+      {
+        Console.WriteLine( "File not found." );
+        return;
+      }
+
+      Scan( path );
+    }
 
     static void Scan( string path )
     {
-      using( var iso = new IsoDevice( path ) )
+      Console.WriteLine( path );
+
+      var isoDevice = new Iso9960Device( path );
+
+      try
       {
-        iso.Initialize();
-
-        bool show = false;
-        var sb = new StringBuilder();
-        sb.AppendFormat( "ROM: {0}\n", path );
-        foreach( var file in iso.EnumerateFiles( true ) )
-        {
-          var foundMagic = FileMagicScanner.TryResolveFileMagic( file.GetStream(), out var magic );
-
-          if( !foundMagic )
-            continue;
-
-          if( magic != FileMagicDefinitions.ELF || !ElfDebugSymbolScanner.HasDebugSymbols( file.GetStream() ) )
-            continue;
-
-          sb.AppendFormat( " - {0} has debug symbols!\n", file.FullPath );
-          show = true;
-        }
-
-        if( !show )
-          return;
-
-        Console.WriteLine( sb.ToString() );
+        isoDevice.Initialize();
+      }
+      catch( Exception ex )
+      {
+        Console.WriteLine( "Failed to read ISO: {0}", ex.Message );
+        return;
       }
 
+      var stopwatch = Stopwatch.StartNew();
+      new Scanner( isoDevice ).Scan();
+      stopwatch.Stop();
 
+      Console.WriteLine( "Finished in {0}.", stopwatch.Elapsed );
     }
 
+
   }
+
 }
---
+++ ScanTool/Scanner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ScanTool.CoreLib.Data;
+using ScanTool.CoreLib.Scanning.Symbols;
+using ScanTool.CoreLib.Tools;
+using ScanTool.CoreLib.VFS;
+
+namespace ScanTool
+{
+
+  public class Scanner
+  {
+
+    #region Data Members
+
+    private readonly VfsDevice _device;
+
+    #endregion
+
+    #region Constructor
+
+    public Scanner( VfsDevice device )
+    {
+      _device = device;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Scan()
+    {
+      if( !_device.IsInitialized )
+        _device.Initialize();
+
+      foreach( var file in _device.EnumerateFiles( true ) )
+        Scan( file );
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void Scan( VfsFile file, int level = 1 )
+    {
+      SetStatus( file.FullPath );
+      var typeMatches = FileScanner.DetermineFileType( file.GetStream() );
+      if( typeMatches.Length == 0 )
+        return;
+
+      var fileType = typeMatches[0].FileType;
+      Console.WriteLine( "{0}- {1}: {2}", new string( ' ', level * 2 ), file.FullPath, fileType.Name );
+
+      if( fileType.Category == FileCategory.Archive
+        && VfsDeviceFactory.TryGetDevice( fileType, file.GetStream(), out var device ) )
+      {
+        try
+        {
+          device.Initialize();
+          foreach( var childFile in device.EnumerateFiles( true ) )
+            Scan( childFile, level + 1 );
+        }
+        catch( Exception ex )
+        {
+          Console.WriteLine( "{0}- Failed to read from the archive: {1}", new string( ' ', ( level + 1 ) * 2 ), ex.Message );
+        }
+      }
+      else if( fileType == FileType.ELF )
+      {
+        var symbols = ElfDebugSymbolScanner.GetDebugSymbolCount( file.GetStream() );
+        if( symbols > 2 )
+          Console.WriteLine( "{0}- {1} Debug Symbols Found", new string( ' ', ( level + 1 ) * 2 ), symbols );
+      }
+    }
+
+    private void SetStatus( string fileName )
+    {
+      Console.Title = $"ScanTool | {fileName}";
+    }
+
+    #endregion
+
+  }
+
+}
