476397b72008e1b7f9cea79ef3e09849de2dfbac
Haus <wildenha@cse.msu.edu>
2020-06-19 16:37:28 -0400

    Add VfsDeviceFactory

---
+++ ScanTool.CoreLib/VFS/VfsDeviceFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using ScanTool.CoreLib.Data;
+using ScanTool.CoreLib.VFS.Devices;
+
+namespace ScanTool.CoreLib.VFS
+{
+
+  public static class VfsDeviceFactory
+  {
+
+    public static bool TryGetDevice( FileType fileType, Stream stream, out VfsDevice device )
+    {
+      device = null;
+
+      if( fileType == FileType.GZip )
+        device = new GZipArchiveDevice( stream );
+      else if ( fileType == FileType.Iso9660 )
+        device = new Iso9960Device( stream );
+      else if (fileType == FileType.Rar )
+        device = new RarArchiveDevice( stream );
+      else if (fileType == FileType.SevenZip )
+        device = new SevenZipArchiveDevice( stream );
+      else if ( fileType == FileType.Tar )
+        device = new TarArchiveDevice( stream );
+      else if ( fileType == FileType.Zip )
+        device = new ZipArchiveDevice( stream );
+
+      if( device == null )
+        return false;
+
+      device.Initialize();
+      return true;
+    }
+
+  }
+
+}
